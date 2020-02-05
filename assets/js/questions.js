c10fa0b0aeb7e2d80c2357af2404d0ab7393eeb6
armande925 <armande.milhouse@gmail.com>
2020-02-04 18:53:36 -0800

    Most recent iteration of Code Quiz

---
+++ assets/js/questions.js
@@ -0,0 +1,17 @@
+const questions = [
+    {
+      question: "Which of the following is NOT a javascript data type?",
+      choices: ["Array", "Object", "Table", "String"],
+      answer: "Table"
+    },
+    {
+      question: "Which parenthesis type is used to invoke functions?",
+      choices: ["{}", "[]", "||", "()"],
+      answer: "()"
+    },
+    {
+      question: "What will this statement return: console.log('7' == 7)",
+      choices: ["true", "false", "maybe", "depends"],
+      answer: "true"
+    }
+  ];
